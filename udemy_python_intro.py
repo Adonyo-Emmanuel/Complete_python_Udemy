e9d5e0f3bdafaa7049730f4d5852151796b73d46
Adonyo Emmanuel <eadonyo@gmail.com>
2021-11-15 13:41:15 +0000

    First day on udemy course - complete python to mastery.

---
+++ udemy_python_intro.py
@@ -0,0 +1,12 @@
+# What is a prgramming langunage?
+'''
+What language does computer speak - binary 1 and 0, on and off, true and false
+some language are lower levels others are higher levles to human languahe
+Assembly - close to machine language
+Python - close to humnans, Javascript, phd, C#
+- Python is an interpeting language (interpretor), goes line by line of code
+- Translater - takes the entire and translate the code to binary into the machine langauge
+For python, the interpeting langauge is cpython
+jython - translator in Javascript
+pypy translation machine written in python.
+'''
