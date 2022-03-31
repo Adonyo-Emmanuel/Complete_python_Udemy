91de74282b18526e835ced38ab3c14590b6b1b14
Adonyo Emmanuel <eadonyo@gmail.com>
2022-03-31 22:21:13 +0100

    Testing the file in pycharm

--- udemy_python_intro.py
+++ udemy_python_intro.py
@@ -1,23 +1,25 @@
-#What is a prgramming langunage?
-'''
+# What is a programming languages?
+""""
 What language does computer speak - binary 1 and 0, on and off, true and false
-some language are lower levels others are higher levles to human languahe
+some language are lower levels others are higher levels to human language
 Assembly - close to machine language
-Python - close to humnans, Javascript, phd, C#
-- Python is an interpeting language (interpretor), goes line by line of code
-- Translater - takes the entire and translate the code to binary into the machine langauge
-For python, the interpeting langauge is cpython
+Python - close to humans, Javascript, phd, C#
+- Python is an interpreting language (interpreter), goes line by line of code
+- Translator - takes the entire and translate the code to binary into the machine language
+For python, the interpreting language is cpython
 jython - translator in Javascript
 pypy translation machine written in python.
 
 Python setup on Machines.
+"""
 
-'''
-def  add(*arg):
+
+def add(*arg):
     return sum(arg)
 
-print(add(1,2,3,4,5,6,7))
 
-print(add(*[10,10,10,10,10,10,10,10]))
+print(add(1, 2, 3, 4, 5, 6, 7))
 
-print(add(**{1:"Adonyo",2:"Emmanuel",2:"Nike",3:"Addidas"}))
+print(add(*[10, 10, 10, 10, 10, 10, 10, 10]))
+
+print("Adonyo Emmanuel")
